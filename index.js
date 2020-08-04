3e963a6a91025413c26a6908e8d5c9799b766337
Parker Williamson <parker.williamson@gmail.com>
2020-08-04 18:24:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function writeCards(names, event) {
   let cards = []
   for (let count = 0; count < names.length; count++) {
-    cards.push(`Thank you ${names} for the $(event) gift!` )
+    cards.push(`Thank you ${names[count]} for the $(event) gift!` )
   }
   return cards 
   }
