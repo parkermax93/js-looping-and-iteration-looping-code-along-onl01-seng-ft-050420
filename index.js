d5653f46aa885b87b06427cbe7a98864b1da9992
Parker Williamson <parker.williamson@gmail.com>
2020-08-04 18:59:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+  let cards = []
+  for (let count = 0; count < names.length; count++) {
+    cards.push(`Thank you, ${names[count]}, for the wonderful ${event} gift!` )
+  }
+  return cards 
+  }
+  
+function countDown(num) {
+    while ( num >= 0 ) {
+      console.log( num-- );
+    }
+}
